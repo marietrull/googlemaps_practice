a344195456e856ac12680c47d9f0830d96afc0a4
marietrull <marietrull@gmail.com>
2018-06-09 11:01:33 -0500

    Added map to the page + zoomed in on specific lat and long coordinates

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('JS file for Google Maps Practice')
+
+// Declare empty variable to store the map
+var map;
+function initMap() {
+	// create a new map with it's enter at specified lat/long
+	// give the map a zoom of 8
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
