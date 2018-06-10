2f593e19a58caf8c45d90e04cdae180875c5c76d
marietrull <marietrull@gmail.com>
2018-06-10 12:08:01 -0500

    Added Geocoder + Pin will display on map based on user input

--- app.js
+++ app.js
@@ -15,31 +15,60 @@ function initMap() {
 		zoom: zoom
 	});
 
+	let geocoder = new google.maps.Geocoder();
+
 	// will put marker on the page where initial Lat Long Coordinates are
-	var marker = new google.maps.Marker({
+	let marker = new google.maps.Marker({
 		position: myLatLng,
 		map: map,
 		title: 'Hello World!'
 	});
+
+	$('button').on('click', (e) => {
+
+		// stop the page from refreshing on click
+		e.preventDefault();
+
+		// grab inputs from the page
+		let latitude = document.getElementById('latitude').value;
+		let longitude = document.getElementById('longitude').value;
+		let zoom = document.getElementById('zoom').value;
+		let newLatLng = {lat: latitude, lng: longitude};
+
+		// show us that the inputs are correct
+		console.log('Button Clicked! Latitude: ' + latitude + ' Longitude: ' + longitude + ' Zoom: ' + zoom);
+
+		geocodeAddress(geocoder, map)
+
+	})
 }
 
 
-$('button').on('click', (e) => {
+//PUT A PIN ON THE MAP AT THE USER'S ADDRESS
+function geocodeAddress(geocoder, map) {
 
-	// stop the page from refreshing on click
-	e.preventDefault();
-
-	// grab inputs from the page
+	//Grab user input and format it in a way that can be run against the chicago API
 	let latitude = document.getElementById('latitude').value;
 	let longitude = document.getElementById('longitude').value;
 	let zoom = document.getElementById('zoom').value;
+	let newLatLng = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
 
-	// show us that the inputs are correct
-	console.log('Button Clicked! Latitude: ' + latitude + ' Longitude: ' + longitude + ' Zoom: ' + zoom);
-	
-	// place marker based on user's input
-
-
-})
+	//Takes the address and formats it in a way that googlemaps can work with
+	geocoder.geocode({'location': newLatLng}, function(results, status) {
+    	if (status === 'OK') {
+      		if (results[0]) {
+        		map.setZoom(11);
+        		var marker = new google.maps.Marker({
+          		position: newLatLng,
+          		map: map
+        		});
+      		} else {
+        		window.alert('No results found');
+      			}
+    	} else {	
+      	window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+}
 
 // Form Handling	
